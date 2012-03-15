60be931b3f02a1d924f1c92f0da4c458b658dd85
Rick Xing <oreofish@gmail.com>
2012-03-15 22:13:13 +0800

    add coffee mode

--- dotemacs.d/el-get/.loaddefs.el
+++ dotemacs.d/el-get/.loaddefs.el
@@ -3,7 +3,22 @@
 ;;; Code:
 
 
-;;;### (autoloads nil nil ("../../../../.emacs.d/el-get/csv-mode/csv-mode.el")
-;;;;;;  (20321 30607 868735))
+;;;### (autoloads nil nil ("../../../../.emacs.d/el-get/coffee-mode/coffee-mode.el")
+;;;;;;  (20321 63416 246261))
+
+;;;***
+
+;;;### (autoloads (coffee-mode) "coffee-mode" "../../../../.emacs.d/el-get/coffee-mode/coffee-mode.el"
+;;;;;;  (20321 63415))
+;;; Generated autoloads from ../../../../.emacs.d/el-get/coffee-mode/coffee-mode.el
+
+(autoload 'coffee-mode "coffee-mode" "\
+Major mode for editing CoffeeScript.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
 
 ;;;***
